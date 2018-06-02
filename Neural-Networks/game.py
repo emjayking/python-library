fd63d58ce5e15f07f5645bac9766cf09657000ee
matthew king <matthewking@Lounges-Mac-Pro.local>
2018-06-02 14:20:53 +1200

    created start of gampe.py for simple game

---
+++ Neural-Networks/game.py
@@ -0,0 +1,9 @@
+#A game where a user controlls a python turtle to reach a goal. avoiding obstacles along the way
+
+#imports
+import turtle as tp
+import random
+
+#Turtle setup
+goal = tp.Turtle()
+goal
