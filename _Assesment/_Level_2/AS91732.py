12645de0f3c4792c7caf6489141cd92b7bf18de8
emjayking <emjayking23@hotmail.co.nz>
2019-02-01 09:22:20 +1300

    1/02/19 9:21AM finished main code, started on puirchase logic

--- _Assesment/_Level_2/AS91732.py
+++ _Assesment/_Level_2/AS91732.py
@@ -2,7 +2,12 @@
 # TO DO: add boundary conditions. baby proof code, plan this $%!#, allow the user to choose their units. rework function
 # Ask user how much cash they have, (must be reasonable amount i.e. $10)
 print('hello world')
-
+# ---------------------------------- Variables ------------------------------- #
+items = {} # dictionary that stores all the information
+average_price = 0 # the average price of all the products
+cheapest_item = ["", 0] # the cheapest item listed
+dearest_item = ["", 0] # the most expensive item listed
+remaining_budget = 0 # this will be used to calculate how many items the user can buy
 # ---------------------------------- Functions ------------------------------- #
 def enter_value(name):
     # basic info for each product
@@ -35,7 +40,7 @@ while True:
     except ValueError:
         print("Error, try again")
 # Loop
-items = {}
+
 for i in range(100): # can't imagine any students using a budget calculator buying more than 100 packs of instant noodles
 
     # Enter product info (name weight/volume, price etc)
@@ -50,17 +55,26 @@ for i in range(100): # can't imagine any students using a budget calculator buyi
 
 print(items)
 # When the user exits the loop, display summary values
-
+for name, values in items.items()
     # Item name and unit price for each item in the list
-
+    print("{} costs ${}".format(name, values["price"]))
     # Avg price
-
+    average_price += values["price"]
     # Cheapest Item
-
+    if values["price"] < cheapest_item[1]:
+        cheapest_item = [name, values["price"]]
     # Most Expensive Item
+    if values["price"] > dearest_item[1]:
+        dearest_item = [name, values["price"]]
 
-# Recomend what item/s to purchase based on
+# ---------------------------------- purchase logic ------------------------- #
 
-    # permutate the items until a combination that maximises volume while   fitting budget is found.
+# calculate the 'value' of the product as a function of cost//mass(kg)
+
+    # convert all units to kg
+
+    # calculate 'value' as 'unit cost'
+
+# recomend the best value for money that fits the user's budget
 
 # Give user option to restart or exit.
