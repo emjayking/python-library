aa137e6f516a97a626498b012205010f1a57e738
emjayking <emjayking23@hotmail.co.nz>
2019-02-03 10:49:04 +1300

    3/02/19 10:48 finished all baic code still need to add boundary values and baby proofing

--- _Assesment/_Level_2/AS91732.py
+++ _Assesment/_Level_2/AS91732.py
@@ -1,6 +1,6 @@
 # 31/01/19, basic plan and starter code
 # TO DO: add boundary conditions. baby proof code, plan this $%!#.
-# Ask user how much cash they have, (must be reasonable amount i.e. $10)
+
 print('hello world')
 # ---------------------------------- Variables ------------------------------- #
 items = {} # dictionary that stores all the information
@@ -9,13 +9,14 @@ cheapest_item = ["", 0] # the cheapest item listed
 dearest_item = ["", 0] # the most expensive item listed
 remaining_budget = 0 # this will be used to calculate how many items the user can buy
 cheapest_unit_cost = ["", 0]
+recomendation = [] # what the user should buy
 # ---------------------------------- Functions ------------------------------- #
 def enter_value(name):
     # basic info for each product
     item = {
-    "price": "",
+    "price": 00.00,
     "unit": "",
-    "mass": "",
+    "mass": 0,
     }
     for value in item: # loop to create a dictionary for the product
         # basic 'if' logic to help with grammar
@@ -31,61 +32,76 @@ def enter_value(name):
     product_values = item
     return product_values
 
+
+def find_cheapest():
+    for name, value in items.items():
+        if values["unit_cost"] < cheapest_unit_cost[1]:
+            cheapest_unit_cost = [name, values["unit_cost"]]
+
 # ------------------------------- Code ------------------------------ #
+    while True:
+    while True:
+        try:
+            budget = float(input("what is your budget?   $:"))
+            #-------- Add conditions for reasonable boundaries -------#
+            break
+        except ValueError:
+            print("Error, try again")
+    # Loop
 
-while True:
-    try:
-        cash = float(input("what is your budget?   $:"))
-        #-------- Add conditions for reasonable boundaries -------#
-        break
-    except ValueError:
-        print("Error, try again")
-# Loop
+    for i in range(100): # can't imagine any students using a budget calculator buying more than 100 packs of instant noodles
 
-for i in range(100): # can't imagine any students using a budget calculator buying more than 100 packs of instant noodles
+        # Enter product info (name weight/volume, price etc)
+        product_name = input("what is the name of the product?")
+        items[product_name] = enter_value(product_name)
 
-    # Enter product info (name weight/volume, price etc)
-    product_name = input("what is the name of the product?")
-    items[product_name] = enter_value(product_name)
+        # Ask to continue
+        confirm = "n"
+        confirm = input("do you want to continue y/n   :")
+        if confirm == "n":
+            break
 
-    # Ask to continue
-    confirm = "n"
-    confirm = input("do you want to continue y/n   :")
-    if confirm == "n":
-        break
+    print(items)
+    # When the user exits the loop, display summary values
+    for name, values in items.items():
+        # Item name and unit price for each item in the list
+        print("{} costs ${}".format(name, values["price"]))
+        # Avg price
+        average_price += values["price"]
+        # Cheapest Item
+        if values["price"] < cheapest_item[1]:
+            cheapest_item = [name, values["price"]]
+        # Most Expensive Item
+        if values["price"] > dearest_item[1]:
+            dearest_item = [name, values["price"]]
 
-print(items)
-# When the user exits the loop, display summary values
-for name, values in items.items():
-    # Item name and unit price for each item in the list
-    print("{} costs ${}".format(name, values["price"]))
-    # Avg price
-    average_price += values["price"]
-    # Cheapest Item
-    if values["price"] < cheapest_item[1]:
-        cheapest_item = [name, values["price"]]
-    # Most Expensive Item
-    if values["price"] > dearest_item[1]:
-        dearest_item = [name, values["price"]]
+    # ---------------------------------- purchase logic ------------------------- #
 
-# ---------------------------------- purchase logic ------------------------- #
+    # calculate the 'value' of the product as a function of cost//mass(kg)
+    for name, values in items.items():
+        # convert all units to kg
+        if values["unit"] == "g" or values["unit"] == "ml":
+            values["mass"] /= 1000
+        # for the sake of my sanity, 1 litre = 1 kilogram
 
-# calculate the 'value' of the product as a function of cost//mass(kg)
-for name, values in items.items():
-    # convert all units to kg
-    if values["unit"] == "g" or values["unit"] == "ml":
-        values["mass"] /= 1000
-    # for the sake of my sanity, 1 litre = 1 kilogram
+        # calculate 'value' as 'unit cost'
+        values["unit_cost"] = values["price"] / values["mass"]
 
-    # calculate 'value' as 'unit cost'
-    values["unit_cost"] = values["price"] / values["mass"]
+        #find the lowest 'unit_cost'
+        if values["unit_cost"] < cheapest_unit_cost[1]:
+            cheapest_unit_cost = [name, values["unit_cost"]]
+    # recomend the best value for money that fits the user's budget
+    remaining_budget = budget
+    for name, values in items.items():
+        if values["unit_cost"] == [cheapest_unit_cost[1]] and values["price"] < remaining_budget:
+            recomendation.append(name)
+            remaining_budget -= values["price"]
+            items.del(name)
+            find_cheapest()
 
-    #find the lowest 'unit_cost'
-    if values["unit_cost"] < cheapest_unit_cost[1]:
-        cheapest_unit_cost = [name, values["unit_cost"]]
-# recomend the best value for money that fits the user's budget
-
-for name, values in items.items():
-    if values["unit_cost"] == [""]
+    print(recomendation)
 
 # Give user option to restart or exit.
+    restart = input("would you like to restart?   y/n:  ")
+    if restart == "n":
+        break
