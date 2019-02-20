5495ee1365826864c7349da5b5ebeca15f8e3622
emjayking <emjayking23@hotmail.co.nz>
2019-02-20 13:36:43 +1300

    20/02/19 finished re-working the purchase logic.

--- _Assesment/_Level_2/AS91732.py
+++ _Assesment/_Level_2/AS91732.py
@@ -2,7 +2,7 @@
 # last edited: 12/02/19
 # purpose : to get credits
 # verion num: 1.0
-# notes: this is version 1.0 because it is the first working version that is fully complete any major changes will increase it to 1.1 and any minor changes will make it 1.01 etc. 
+# notes: this is version 1.0 because it is the first working version that is fully complete any major changes will increase it to 1.1 and any minor changes will make it 1.01 etc.
 print('hello world')
 # ---------------------------------- Variables ------------------------------- #
 items = {} # dictionary that stores all the information
@@ -10,7 +10,6 @@ average_price = 0 # the average price of all the products
 cheapest_item = ["", 0] # the cheapest item listed
 dearest_item = ["", 0] # the most expensive item listed
 remaining_budget = 0 # this will be used to calculate how many items the user can buy
-cheapest_unit_cost = ["", 0]
 recomendation = [] # what the user should buy
 units_of_measurement = ["kg", "g", "ml", "l"]
 # ---------------------------------- Functions ------------------------------- #
@@ -27,13 +26,13 @@ def enter_value(name):
             try:
                 if value == "unit":
                     item[value] = str(input("please choose between kg, g, ml or l to use as the unit of measurement for this product  ").strip())
-                    if item[value] in units_of_measurement:
+                    if item[value] not in units_of_measurement:
                         print("that is not an acceptable unit, please try again")
                         continue
 
 
                 elif value == "mass": #
-                    item[value] = int(input("how many {}s are in the product?".format(item["unit"])))
+                    item[value] = float(input("how many {}s are in the product?".format(item["unit"])))
 
                 elif value == "price":
                     item[value] = float(input("what is the price of the product?  $:"))
@@ -51,12 +50,10 @@ def enter_value(name):
     product_values = item
     return product_values
 
-
-def find_cheapest():
-    for name, value in items.items():
-        if values["unit_cost"] < cheapest_unit_cost[1]:
-            cheapest_unit_cost = [name, values["unit_cost"]]
-
+# this will allow me to sort this list to find the cheapest unit cost
+def my_key(dict):
+    name, values = dict.values()
+    return values["unit_cost"]
 # ------------------------------- Code ------------------------------ #
 while True:
     while True:
@@ -99,7 +96,7 @@ while True:
         if values["price"] > dearest_item[1]:
             dearest_item = [name, values["price"]]
 
-    # ---------------------------------- purchase logic ------------------------- #
+    # ---------------------------------- purchase logic ---------------------- #
 
     # calculate the 'value' of the product as a function of cost//mass(kg)
     for name, values in items.items():
@@ -109,23 +106,22 @@ while True:
         # for the sake of my sanity, 1 litre = 1 kilogram
 
         # calculate 'value' as 'unit cost'
-        values["unit_cost"] = values["price"] / values["mass"]
+        values["unit_cost"] = float(values["price"] / values["mass"])
+        print("unit costs {}".format(values["unit_cost"]))
 
-        #find the lowest 'unit_cost'
-        if values["unit_cost"] < cheapest_unit_cost[1]:
-            cheapest_unit_cost = [name, values["unit_cost"]]
+    #find the lowest 'unit_cost'
+    print("unsorted {}".format(items))
+    items.sort(key=my_key)
+    print("sorted {}".format(items))
     # recomend the best value for money that fits the user's budget
     remaining_budget = budget
+    # loop through each items
     for name, values in items.items():
-        if values["unit_cost"] == [cheapest_unit_cost[1]] and values["price"] < remaining_budget:
+        # if the item costs more than the budgeet then ignore it
+        if values['price'] < remaining_budget:
             recomendation.append(name)
             remaining_budget -= values["price"]
-            del items["name"]
-            find_cheapest()
-    if recomendation != []:
-        print("I would recomend you buy:" + recomendation)
-    else:
-        print("You can't buy any of the products you entered with the budget you have")
+    print(recomendation)
 
 # Give user option to restart or exit.
     restart = input("would you like to restart?   y/n:  ")
