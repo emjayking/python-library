df59c408e8672d2b1fa2bf50484ecd53b26855ca
emjayking <emjayking23@hotmail.co.nz>
2019-03-02 21:12:08 +1300

    rough planning complete

--- chemistry_calculator.py
+++ chemistry_calculator.py
@@ -1,1 +1,25 @@
-# purpose: calculate the mass, molar mass and mols of a molecule when given an equation and another molecule
+# purpose: calculate the mass, molar mass and mols of a molecule when given an equation and another molecule. I might try do this with tkinter so I can ue it for the Level 3 GUI project.
+
+# planning
+
+# dictionary of all elements and their relative molecular mass
+
+# ask user for balanced equation
+
+    # develop way of checking if equation is balanced
+
+# Ask for the mass of the first molecule
+
+# calculate the molecular mass (M) of the molecule
+
+# convert mass to mols (n = m / M)
+
+# Ask for the second molecule (use function from first molecule?)
+
+# find molar ratio of first to second molecule
+
+# calculate mols of second molecule
+
+# convert mols to mass (m = M * n)
+
+# print(results)
