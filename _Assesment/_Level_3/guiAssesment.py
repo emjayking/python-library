780ec87886ee3a528a9a272e678b5632b098832b
emjayking <emjayking23@hotmail.co.nz>
2019-03-07 13:49:21 +1300

    FINISHED CODE

--- _Assesment/_Level_3/guiAssesment.py
+++ _Assesment/_Level_3/guiAssesment.py
@@ -39,6 +39,7 @@ super_label_text = StringVar()
 lizard_label_text = StringVar()
 water_label_text = StringVar()
 status_str = StringVar()
+comics_sold = IntVar()
 stocks = [8, 12, 3]
 comics = ["Super Dude", "Lizard Man", "Water Woman"]
 
@@ -49,15 +50,19 @@ super_label_text.set("Super Dude Stock: {}".format(stocks[0]))
 status_str.set("Startup Succesfull")
 
 # -------- Frame Setup ----------- #
-
-status_frame = ttk.Frame(root)
-status_frame.grid(row=2, column=0, sticky="NSEW", padx=10, pady=5)
-
 sell_frame = ttk.Frame(root)
-sell_frame.grid(row=0, column=0, sticky="NSEW", padx=10, pady=5)
+sell_frame.grid(row=0, column=0, sticky="NS", padx=10, pady=5)
 
 stock_frame = ttk.Frame(root)
-stock_frame.grid(row=1, column=0, sticky="NSEW", padx=10, pady=5)
+stock_frame.grid(row=1, column=0, sticky="NS", padx=10, pady=5)
+
+restock_frame = ttk.Frame(root)
+restock_frame.grid(row=3, column=0, sticky="NSEW", padx=10, pady=5)
+
+status_frame = ttk.Frame(root)
+status_frame.grid(row=4, column=0, sticky="NS", padx=10, pady=5)
+
+
 
 # -------- Functions ------------ #
 def sell():
@@ -84,7 +89,41 @@ def sell():
         else:
             status_str.set("Sell attempt failed, stock is at 0")
 
+def restock():
+    amount = int(restock_num.get())
+    comic = chosen_option.get()
+    if comic == "Super Dude":
+        if amount > 0:
+            stocks[0] += amount
+            super_label_text.set("Super Dude Stock: {}".format(stocks[0]))
+            status_str.set("Restock attempt succesfull")
+        else:
+            status_str.set("restock attempt failed, cannot restock a negative amount")
+    elif comic == "Lizard Man":
+        if amount > 0:
+            stock[s1] += amount
+            lizard_label_text.set("Lizard Man Stock: {}".format(stocks[1]))
+            status_str.set("Restock attempt succesfull")
+        else:
+            status_str.set("restock attempt failed, cannot restock a negative amount")
+    else:
+        if amount > 0:
+            stocks[2] += amount
+            water_label_text.set("Water Woman Stock: {}".format(stocks[2]))
+            status_str.set("Restock attempt succesfull")
+        else:
+            status_str.set("Restock attempt failed, stock is at 0")
+
+
 # ------------------ GUI code main---------------------------- #
+
+label1 = Label(sell_frame, text="Comic: ")
+label1.grid(row=0, column=0, padx=10, pady=5)
+
+comic_selector = ttk.OptionMenu(sell_frame, chosen_option, comics[0], * comics)
+comic_selector.grid(row=0, column=1, padx=10, pady=5)
+
+# ------ Stocks ------ #
 
 super_dude_stock = Label(stock_frame, textvariable=super_label_text)
 super_dude_stock.grid(row=1, column=0, padx=10, pady=5)
@@ -95,20 +134,30 @@ lizard_man_stock.grid(row=2, column=0, padx=10, pady=5)
 water_woman_stock = Label(stock_frame, textvariable=water_label_text)
 water_woman_stock.grid(row=3, column=0, padx=10, pady=5)
 
-label1 = Label(sell_frame, text="Comic: ")
-label1.grid(row=0, column=0, padx=10, pady=5)
-
-comic_selector = ttk.OptionMenu(sell_frame, chosen_option, comics[0], * comics)
-comic_selector.grid(row=0, column=1, padx=10, pady=5)
+# ------ Sell -------- #
 
 sell_button = Button(
 sell_frame, text="Sell", command=sell, activebackground="blue", state="normal"
 )
 sell_button.grid(row=0, column=2, padx=10, pady=5)
 
+# ------ Restock ----- #
+
+restock_label = Label(restock_frame, text="Amount to restock: ")
+restock_label.grid(row=0, column=0, padx=5, pady=5)
+
+restock_entry = Entry(restock_frame, textvariable=restock_num)
+restock_entry.grid(row=0, column=1, padx=2, pady=5)
+
+restock_button = Button(restock_frame, text="Restock", command=restock, activebackground="blue", state="normal")
+restock_button.grid(row=0, column=3, padx=10, pady=5)
+
+# ------- Status ----- #
 
 status = Label(status_frame, textvariable=status_str)
 status.grid(row=0, column=0, padx=10, pady=5)
 
-# ----------------- Stuff n Things
+amount_sold = Label(status_frame, textvariable=comics_sold)
+
+# ----- Stuff n Things ----- #
 root.mainloop()
